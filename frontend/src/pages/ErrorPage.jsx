31f467c071828000ebd7371a2da095e1db9fbada
Fransi-Kurti <you@example.com>
2024-12-03 18:11:24 +0100

    fixing ErrorPage

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -2,12 +2,16 @@ import MainNavigation from "../components/MainNavigation";
 import Header from "../components/Header";
 
 export default function ErrorPage() {
-    return <>
-    <Header />
-    <MainNavigation />
-    <main style={{textAlign: 'center'}}>
-        <h1>Am error occured!</h1>
+  return (
+    <>
+      <Header />
+      <MainNavigation />
+      <main
+        style={{ textAlign: "center", color: "white", backgroundColor: "red" }}
+      >
+        <h1>An error occurred!</h1>
         <p>Could not find this page!</p>
-    </main>
+      </main>
     </>
+  );
 }
